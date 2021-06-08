1ff4908c083ec9946a9ee27c1e31055b05fc3f44
Ameer Sabri <ameer.a.sabri@gmail.com>
2021-06-09 00:47:28 +1000

    Razed current tests

--- NameSorter.Tests/Sorter_Tests.cs
+++ NameSorter.Tests/Sorter_Tests.cs
@@ -6,21 +6,6 @@ namespace NameSorter.Tests
     [TestFixture]
     public class Sorter_Tests
     {
-        //private string testDataPath = TestContext.CurrentContext.TestDirectory + "\\test_data\\";
 
-        //[TestCase("OneNameUnsorted.txt", "OneNameExpected.txt")]
-        //[TestCase("TenNamesUnsorted.txt", "TenNamesExpected.txt")]
-        //[TestCase("OneHundredNamesUnsorted.txt", "OneHundredNamesExpected.txt")]
-        //[TestCase("OneThousandNamesUnsorted.txt", "OneThousandNamesExpected.txt")]
-        //public void VariousSizedUnsortedLists_CanProcessInSorter(string testFile, string testExpectedFile)
-        //{
-        //    var testInputFile = testDataPath + testFile;
-        //    var testOutputFile = testDataPath + testExpectedFile;
-
-        //    var testInputFileProcessed = Sorter.Sort(testInputFile, "output.txt").Trim();
-        //    var testOutputFileRead = File.ReadAllText(testOutputFile).Trim();
-
-        //    Assert.AreEqual(testInputFileProcessed, testOutputFileRead);
-        //}
     }
 }
